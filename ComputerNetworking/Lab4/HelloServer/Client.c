723bb86113495d3e7dffa2f954947edbdaed4328
Armaan-04 <alamarmaan2004@gmail.com>
2025-08-19 01:43:39 +0530

    Server&Client

---
+++ ComputerNetworking/Lab4/HelloServer/Client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 8080
+
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    char *message = "Hello Server";
+
+    sendto(sockfd, message, strlen(message), 0,
+           (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    printf("Message sent: %s\n", message);
+
+    close(sockfd);
+    return 0;
+}
---
+++ ComputerNetworking/Lab4/HelloServer/Server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t len;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;      // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        exit(1);
+    }
+
+    printf("UDP server is waiting for messages...\n");
+
+    len = sizeof(cliaddr);
+    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                     (struct sockaddr *)&cliaddr, &len);
+    buffer[n] = '\0';
+    printf("Client: %s\n", buffer);
+
+    close(sockfd);
+    return 0;
+}
